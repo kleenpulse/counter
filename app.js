ec2354e8a2615930e8dfa2037b9829ed94b42ff6
kleenpulse <skyeparagon@gmail.com>
2022-11-09 20:41:44 +0100

    fix

--- app.js
+++ app.js
@@ -6,32 +6,24 @@ let timerId
 let minusId
 let pressDuration = 5
 
+addBtn.addEventListener('touchstart', addCounter, false);
 
-addBtn.onclick = function () {
-  count++
-}
-var element;
-addBtn.addEventListener('touchstart', function (e) {
+function addCounter(e) {
   this.textContent = '+'
-  e.preventDefault();
-  // var touch = e.touches[0];
-  addCounter()
-
-}, false);
-
-function addCounter() {
+  // e.preventDefault();
   clearInterval(minusId)
   timerId = setInterval(function () {
     count++
-    if( count >= 20000){
+    countTxt.textContent = `increasing: ${count}`
+    if (count >= 20000) {
       clearInterval(timerId)
+      setTimeout(function () {
+        countTxt.textContent = `Max reached: 20k`
+      }, 1000)
     }
-    countTxt.textContent = `counting....: ${count}`
-    
   }, pressDuration)
 }
 function minusCounter() {
-
   clearInterval(timerId)
   timerId = null
   if (count > 0) {
@@ -42,9 +34,11 @@ function minusCounter() {
 
       if (count < 1) {
         clearInterval(minusId)
-        countTxt.textContent = `count: zero`
-        addBtn.textContent = '+'
+        setTimeout(function () {
+          countTxt.textContent = `min reached: zero`
 
+        }, 1000)
+        addBtn.textContent = '+'
       }
     }, (pressDuration + 5))
   }
@@ -52,17 +46,27 @@ function minusCounter() {
 addBtn.ontouchend = function () {
   minusCounter()
 }
-
-
 addBtn.addEventListener('mousedown', function (e) {
   e.preventDefault();
   if (e.button === 0) {
     this.textContent = '+'
-
     addCounter()
   }
 }, false);
 
 addBtn.onmouseup = function () {
   minusCounter()
+}
+addBtn.onmousemove = function () {
+  window.clearInterval(timerId)
+}
+
+function customMsg(msg, time) {
+  var styler = document.createElement("div")
+  styler.classList.add('custom-msg')
+  styler.innerHTML = `<h1 id="copied"> ${msg} </h1>`
+  setTimeout(function () {
+    styler.parentNode.removeChild(styler)
+  }, time)
+  document.body.appendChild(styler)
 }
